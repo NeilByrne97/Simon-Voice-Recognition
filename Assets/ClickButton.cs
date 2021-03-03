77929a252932bc4633c61c4d49da0149396a6dc9
Neil Byrne <G00343624@gmit.ie>
2021-03-03 18:53:24 +0000

    Buttons change shade when pressed

---
+++ Assets/ClickButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour
+{
+
+    public Material lightMat;
+    public Material normalMat;
+
+    private Renderer myR;
+
+    private void Awake()
+    {
+        myR = GetComponent<Renderer>();
+        myR.enabled = true;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        ClickedColor();
+    }
+
+    private void OnMouseUp()
+    {
+        UnClickedColor();
+    }
+
+    public void ClickedColor()
+    {
+        myR.sharedMaterial = normalMat;
+    }
+
+    public void UnClickedColor()
+    {
+        myR.sharedMaterial = lightMat;
+    }
+
+}
