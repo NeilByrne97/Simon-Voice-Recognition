2274dcef7a3e0e18cce2f25a656bab662cc35b11
Neil Byrne <G00343624@gmit.ie>
2021-03-20 20:20:14 +0000

    Difficulty menu linked to scenes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame ()
+  /*  public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+ 1);
+    }
+*/
+    public void EasyLevel ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+ 1);
+    }
+
+    public void MediumLevel ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+ 2);
+    }
+
+    public void HardLevel ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+ 3);
     }
 
     public void QuitGame()
